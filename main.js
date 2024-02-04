da2588ac17aed43ec47ab808fe3c628d77fdfd7e
ardalanmemar93 <ardalanmemar@gmail.com>
2024-02-03 17:37:14 -0800

    threejs practice

--- main.js
+++ main.js
@@ -47,6 +47,25 @@ const controls = new OrbitControls(camera, renderer.domElement);
 
 
 
+function addStar() {
+	const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+	const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	const star = new THREE.Mesh(geometry, material);
+
+	const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+	star.position.set(x, y, z);
+	scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+// Add a background
+const spaceTexture = new THREE.TextureLoader().load('dark-space.jpeg');
+scene.background = spaceTexture;
+
+
+
 function animate() {
 	requestAnimationFrame(animate);
 	torus.rotation.x += 0.01;
